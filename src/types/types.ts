9cf72c8d34e54e20d303b6772f99e885b4e62fcf
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-02-16 23:39:18 +0900

    [update] SSG で Supabase のデータを取得する

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+export type Task = {
+  id: string;
+  created_at: string;
+  title: string;
+  user_ui: string | undefined;
+};
+
+export type Notice = {
+  id: string;
+  created_at: string;
+  content: string;
+  user_ui: string | undefined;
+}
