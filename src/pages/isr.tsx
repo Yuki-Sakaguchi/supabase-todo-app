90f405643ad1d25e69e28cba588f8e9256b0df92
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-02-17 22:31:04 +0900

    [update] isr を追加

---
+++ src/pages/isr.tsx
@@ -0,0 +1,66 @@
+/**
+ * Supabase のデータを使って isr でデータを組み込んだページを作る
+ */
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { NextPage, GetStaticProps } from 'next';
+
+import { Layout } from '@/components/Layout';
+import { supabase } from '@/utils/supabase';
+import { Task, Notice } from '@/types/types';
+
+export const getStaticProps: GetStaticProps = async () => {
+  console.log('getStaticProps/isr invoked');
+  const { data: tasks } = await supabase
+    .from('todos')
+    .select('*')
+    .order('created_at', { ascending: true });
+  const { data: notices } = await supabase
+    .from('notices')
+    .select('*')
+    .order('created_at', { ascending: true });
+  return { props: { tasks, notices }, revalidate: 5 };
+};
+
+type StaticProps = {
+  tasks: Task[];
+  notices: Notice[];
+};
+
+const Isr: NextPage<StaticProps> = ({ tasks, notices }) => {
+  const router = useRouter();
+  return (
+    <Layout title="ISR">
+      <p className="mb-3 text-indigo-500">ISR</p>
+      <ul className="mb-3">
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <p className="text-lg font-extrabold">{task.title}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="mb-3">
+        {notices.map((notice) => {
+          return (
+            <li key={notice.id}>
+              <p className="text-lg font-extrabold">{notice.content}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <Link href="/ssr" prefetch={false}>
+        <span className="mb-3 text-xs">Link to ssr</span>
+      </Link>
+      <Link href="/isr" prefetch={false}>
+        <span className="mb-3 text-xs">Link to isr</span>
+      </Link>
+      <button className="mb-3 text-xs" onClick={() => router.push('/ssr')}>
+        Route to ssr
+      </button>
+    </Layout>
+  );
+};
+
+export default Isr;
