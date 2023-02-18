11a9c525a03e1655b6ff4621b023ddd6d87017f3
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-02-18 21:54:02 +0900

    [update] ログイン処理の準備

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps, NextWebVitalsMetric } from 'next/app';
 
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { supabase } from '@/utils/supabase';
+
 /**
  * web vitals を計測するための処理
  */
@@ -27,8 +33,48 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
   }
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { push, pathname } = useRouter();
+
+  // ログインしていなかったらトップに飛ばす処理
+  const validateSession = async () => {
+    const user = supabase.auth.user();
+    if (user && pathname === '/') {
+      push('/dashboard');
+    } else if (!user && pathname !== '/') {
+      await push('/');
+    }
+  };
+
+  // ログイン状態のセッションの変更を検知して動く処理
+  supabase.auth.onAuthStateChange((event, _) => {
+    if (event === 'SIGNED_IN' && pathname === '/') {
+      push('/dashboard');
+    }
+    if (event === 'SIGNED_OUT') {
+      push('/');
+    }
+  });
+
+  useEffect(() => {
+    validateSession();
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
